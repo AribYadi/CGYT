0eb879c723fdb963d38bf539034697e3396afbaf
AribYadi <aribyadi@gmail.com>
2022-11-02 04:33:05 +0700

    open window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-  println!("Hello, world!");
+use macroquad::prelude::*;
+
+fn window_conf() -> Conf {
+  Conf { window_title: "Game Off 2022".to_string(), ..Default::default() }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+  loop {
+    clear_background(WHITE);
+
+    next_frame().await;
+  }
 }
